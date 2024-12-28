7e6780d5db0dd9f2b0971bdf454413da974f0105
zeyten88 <168464999+zeyten88@users.noreply.github.com>
2024-12-28 03:09:57 +0100

    Update init.lua

--- hyprland-dots/.config/nvim/init.lua
+++ hyprland-dots/.config/nvim/init.lua
@@ -1,123 +1,75 @@
--- lazy.nvim, Zeyten.
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+-- Zeyten's Neovim Config.
+
+-- Added Cd: to switch directories.
+-- Implemented nvim-tree
+
+if not pcall(require, "lazy") then
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+    })
+  end
+  vim.opt.rtp:prepend(lazypath)
 end
-vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   {
     'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      -- Disable netrw as recommended by Nvim-Tree
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
-      -- Setup Nvim-Tree
       require("nvim-tree").setup({
         view = { width = 30, side = "left" },
-        renderer = {
-          highlight_git = true,
-          icons = {
-            show = {
-              git = true,
-              folder = true,
-              file = true,
-            },
-          },
-        },
+        sync_root_with_cwd = true,
         filters = { dotfiles = false },
       })
 
-      -- Open Nvim-Tree on startup
+      vim.api.nvim_create_user_command('Tree', 'NvimTreeToggle', { nargs = 0 })
+
+      vim.api.nvim_create_user_command('CdTree', function(opts)
+        local path = opts.args
+        path = vim.fn.expand(path)
+
+        if vim.fn.isdirectory(path) == 1 then
+          vim.cmd("cd " .. path)
+          require("nvim-tree.api").tree.change_root(path)
+          print("Changed directory to " .. path)
+        else
+          print("Invalid directory: " .. path)
+        end
+      end, { nargs = 1 })
+
       local function open_nvim_tree()
-        -- Prevent opening Nvim-Tree if editing a file or a directory is already open
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if bufname == "" and vim.fn.isdirectory(vim.fn.getcwd()) == 1 then
+        if vim.api.nvim_buf_get_name(0) == "" and vim.fn.isdirectory(vim.fn.getcwd()) == 1 then
           require("nvim-tree.api").tree.open()
         end
       end
-
-      vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
-      -- Create a custom :Tree command
-      vim.api.nvim_create_user_command(
-        'Tree',                            -- Command name
-        'NvimTreeToggle',                  -- The action (toggle Nvim-Tree)
-        { nargs = 0 }                      -- No arguments needed
-      )
-    end,
-  },
-})
--- lazy.nvim setup (if not already done)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    config = function()
-      -- Disable netrw as recommended by Nvim-Tree
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-
-      -- Setup Nvim-Tree
-      require("nvim-tree").setup({
-        view = { width = 30, side = "left" },
-        renderer = {
-          highlight_git = true,
-          icons = {
-            show = {
-              git = true,
-              folder = true,
-              file = true,
-            },
-          },
-        },
-        filters = { dotfiles = false },
-      })
-
-      -- Open Nvim-Tree on startup
-      local function open_nvim_tree()
-        -- Prevent opening Nvim-Tree if editing a file or a directory is already open
-        local bufname = vim.api.nvim_buf_get_name(0)
-        if bufname == "" and vim.fn.isdirectory(vim.fn.getcwd()) == 1 then
-          require("nvim-tree.api").tree.open()
-        end
-      end
-
-      vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
-      -- Create a custom :Tree command
-      vim.api.nvim_create_user_command(
-        'Tree',                            -- Command name
-        'NvimTreeToggle',                  -- The action (toggle Nvim-Tree)
-        { nargs = 0 }                      -- No arguments needed
-      )
+      vim.api.nvim_create_autocmd("VimEnter", { callback = open_nvim_tree })
     end,
   },
 })
 
+vim.cmd([[
+  set wildmenu
+  set wildmode=longest:full,full
+  command! -nargs=1 -complete=dir Cd call ChangeDirWithCompletion(<q-args>)
+
+  function! ChangeDirWithCompletion(path)
+    let path = expand(a:path)
+
+    if isdirectory(path)
+      execute 'cd ' . path
+      echo 'Changed directory to ' . path
+    else
+      echo 'Invalid directory: ' . path
+    endif
+  endfunction
+]])
